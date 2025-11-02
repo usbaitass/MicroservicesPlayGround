ae1063b274376450070a0d9096b842d812937ed5
Ulan Baitassov <usbaitass@gmail.com>
2025-11-02 17:51:44 +0500

    update

--- angular-app/src/app/components/send-message/send-message.ts
+++ angular-app/src/app/components/send-message/send-message.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { HttpClientService } from '../../networks/http-client.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-send-message',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './send-message.html',
 })
 export class SendMessage {
+  http = inject(HttpClientService);
 
+  messageText: string = '';
+
+  public sendMessage() {
+    const message = {
+      content: this.messageText,
+    };
+    this.http.post(`/messages`, message).subscribe();
+  }
 }
