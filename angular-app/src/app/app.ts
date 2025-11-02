973b6d1b02f6cd8f9872b31fa8499366aae63f35
Ulan Baitassov <usbaitass@gmail.com>
2025-11-02 15:47:39 +0500

    update

--- angular-app/src/app/app.ts
+++ angular-app/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Messages } from './components/messages/messages';
+import { SendMessage } from './components/send-message/send-message';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Messages, SendMessage],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
---
+++ angular-app/src/app/components/send-message/send-message.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-send-message',
+  imports: [],
+  templateUrl: './send-message.html',
+})
+export class SendMessage {
+
+}
